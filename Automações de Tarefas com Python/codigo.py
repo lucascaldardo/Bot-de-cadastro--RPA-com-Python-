24f6d8b9104c09762f3be4f52b43ccf68462d100
Lucas Caldardo Pereira <llucascalldardo@gmail.com>
2026-01-15 20:57:55 -0300

    Update codigo.py

--- Automações de Tarefas com Python/codigo.py
+++ Automações de Tarefas com Python/codigo.py
@@ -4,10 +4,12 @@ import pandas
 pyautogui.PAUSE = 0.5
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 tabela = pandas.read_csv("C:/Users/calld/OneDrive/Documentos/GitHub/Bot-de-cadastro--RPA-com-Python-/Automações de Tarefas com Python/produtos.csv")
+
 #Entrar no sistema
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+
 #Fazer login
 pyautogui.write(link)
 pyautogui.press("enter")
@@ -18,6 +20,7 @@ pyautogui.press("tab")
 pyautogui.write("1234")
 pyautogui.press("enter")
 time.sleep(3)
+
 #Cadastrar produtos
 for linha in tabela.index:
     pyautogui.click(x=974, y=324)
